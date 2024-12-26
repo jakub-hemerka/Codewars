4cb8c20744e489e5d6534f365f282c558e8d8183
jakub-hemerka <jhemi@outlook.cz>
2024-12-26 21:26:39 +0100

    Backing up

---
+++ PowerTowerModulo/Kata.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace PowerTowerModulo;
+public class Kata
+{
+    public static int Tower(BigInteger b, BigInteger h, BigInteger m)
+    {
+        return 0;
+    }
+}
